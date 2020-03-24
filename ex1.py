636154589e6b52f98810dff35ae6771b863e1528
fecke.smile@hotmail.it <fecke.smile@hotmail.it>
2020-03-24 14:40:06 +0100

    ex1_done.

--- ex1.py
+++ ex1.py
@@ -3,21 +3,23 @@ import sys
 from Bio.SVDSuperimposer import SVDSuperimposer
 import numpy as np
 
-def get_ca_atoms(pdbfile,chain,rlist,atom='CA'):
+
+def get_ca_atoms(pdbfile, chain, rlist, atom='CA'):
     l_coords = []
     f_pdb = open(pdbfile, 'r')
     for line in f_pdb:
-        if line[:4] != 'ATOM' : continue
-        if line[21] != chain : continue
-        if line[22:26].strip() not in rlist : continue
-        if atom not in line : continue
+        if line[:4] != 'ATOM': continue
+        if line[21] != chain: continue
+        if line[22:26].strip() not in rlist: continue
+        if atom not in line: continue
         x = float(line[30:38])
         y = float(line[38:46])
         z = float(line[46:54])
-        l_coords.append([x,y,z])
+        l_coords.append([x, y, z])
     return l_coords
 
-def rmsd(lcoord1, lcoord2):
+
+def rmsd_value(lcoord1, lcoord2):
     if len(lcoord1) != len(lcoord2):
         print(sys.stderr.write("error: number of coords are different\n"))
         sys.exit()
@@ -32,15 +34,12 @@ def rmsd(lcoord1, lcoord2):
         return rmsd
 
 
-
-
-
-if __name__ == '__main__' :
+if __name__ == '__main__':
     # if you call it from the terminal it will execute the program,
     # if it is imported in another file as a module
     # then it will not be executed
     pdbfile1 = sys.argv[1]
-    chain1 = sys.argv[2] # sys.argv[#] makes the program read the input
+    chain1 = sys.argv[2]  # sys.argv[#] makes the program read the input
     pdbfile2 = sys.argv[3]
     chain2 = sys.argv[4]
     list1 = sys.argv[5].split(',')
@@ -48,4 +47,4 @@ if __name__ == '__main__' :
     coord1 = get_ca_atoms(pdbfile1, chain1, list1)
     coord2 = get_ca_atoms(pdbfile2, chain2, list2)
     print(coord1, '\n', coord2)
-    print(rmsd(coord1, coord2))
+    print(rmsd_value(coord1, coord2))
