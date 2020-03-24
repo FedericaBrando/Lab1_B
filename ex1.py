84a6fe7fb35c6e428d7e4b44ef7edfe199e938b3
fecke.smile@hotmail.it <fecke.smile@hotmail.it>
2020-03-24 14:36:41 +0100

    ex1_done.

--- ex1.py
+++ ex1.py
@@ -1,5 +1,7 @@
 #!/usr/bin/env python3.7
 import sys
+from Bio.SVDSuperimposer import SVDSuperimposer
+import numpy as np
 
 def get_ca_atoms(pdbfile,chain,rlist,atom='CA'):
     l_coords = []
@@ -9,8 +11,29 @@ def get_ca_atoms(pdbfile,chain,rlist,atom='CA'):
         if line[21] != chain : continue
         if line[22:26].strip() not in rlist : continue
         if atom not in line : continue
-        l_coords.append(line[30:54].rstrip().split())
+        x = float(line[30:38])
+        y = float(line[38:46])
+        z = float(line[46:54])
+        l_coords.append([x,y,z])
     return l_coords
+
+def rmsd(lcoord1, lcoord2):
+    if len(lcoord1) != len(lcoord2):
+        print(sys.stderr.write("error: number of coords are different\n"))
+        sys.exit()
+    else:
+        svd = SVDSuperimposer()
+        svd.set(np.array(lcoord1), np.array(lcoord2))
+        svd.run()
+        rmsd = svd.get_rms()
+        rot, tran = svd.get_rotran()
+        print('R', rot)
+        print('T', tran)
+        return rmsd
+
+
+
+
 
 if __name__ == '__main__' :
     # if you call it from the terminal it will execute the program,
@@ -25,3 +48,4 @@ if __name__ == '__main__' :
     coord1 = get_ca_atoms(pdbfile1, chain1, list1)
     coord2 = get_ca_atoms(pdbfile2, chain2, list2)
     print(coord1, '\n', coord2)
+    print(rmsd(coord1, coord2))
